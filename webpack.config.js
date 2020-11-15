d987483d0dbd6a99fc41327e4eb2ecd062605782
Metafaux <alex@metafaux.com>
2020-11-14 16:31:22 -0800

    Configure Webpack dev server

---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+const CopyWebpackPlugin = require('copy-webpack-plugin')
+const HTMLWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+  entry: './src/index.ts',
+  mode: 'development',
+  resolve: {
+    extensions: ['.ts', '.js', '.json']
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: [{ loader: 'awesome-typescript-loader'}],
+        exclude: /node_modules/
+      }
+    ]
+  },
+  devServer: {
+    contentBase: 'dist',
+    port: 3000,
+  },
+  devtool: 'inline-source-map',
+  plugins: [
+    new CopyWebpackPlugin({
+      patterns: [
+        {
+          from: 'build/assets',
+          to: 'assets',
+        },
+      ],
+    }),
+    new HTMLWebpackPlugin({
+      template: 'build/index.html',
+      filename: 'index.html',
+    }),
+  ],
+}
